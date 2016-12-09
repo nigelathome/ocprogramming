d1d8ea5898f627129f2e8d39187189c353f31e58
Nigel Lee <lihui8@le.com>
2016-12-09 11:48:24 +0800

    add object MyObject

---
+++ ocprogramming/MyObject.h
@@ -0,0 +1,16 @@
+//
+//  MyObject.h
+//  ocprogramming
+//
+//  Created by Nigel Lee on 09/12/2016.
+//  Copyright © 2016 Apress. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MyObject : NSObject
+{
+    int val0;
+    int val1;
+}
+@end
---
+++ ocprogramming/MyObject.m
@@ -0,0 +1,13 @@
+//
+//  MyObject.m
+//  ocprogramming
+//
+//  Created by Nigel Lee on 09/12/2016.
+//  Copyright © 2016 Apress. All rights reserved.
+//
+
+#import "MyObject.h"
+
+@implementation MyObject
+
+@end
